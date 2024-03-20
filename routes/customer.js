a08df0ab0944c9d33fea8a1a6670e77c0c02ed2c
AliElamir <ali.elamiir.a@gmail.com>
2024-03-20 20:51:57 +0300

    create routes

--- routes/customer.js
+++ routes/customer.js
@@ -5,38 +5,3 @@ const customerCtrl = require('../controllers/customer')
 
 module.exports = router
 
-// router.post('/login', controller.Login)
-// router.post('/register', controller.Register)
-// router.put(
-//   '/update/:user_id',
-//   middleware.stripToken,
-//   middleware.verifyToken,
-//   controller.UpdatePassword
-// )
-// router.get(
-//   '/session',
-//   middleware.stripToken,
-//   middleware.verifyToken,
-//   controller.CheckSession
-// )
-// router.get(
-//   '/userinfo/:id',controller.getUserInfo
-// )
-// router.put('/userupdate/:userid', controller.UpdateUser)
-
-// //get all plants
-// router.get('/', plantCtrl.index)
-
-// //get specific plant
-// router.get('/:id', plantCtrl.show)
-
-// //create a plant
-// router.post('/', plantCtrl.create)
-
-// //delete a plant
-// router.delete('/:id', plantCtrl.deletePlant)
-
-// //update a plant
-// router.put('/:id', plantCtrl.update)
-
-// module.exports = router
---
+++ routes/package.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+const packageCtrl = require('../controllers/package')
+
+//get all packages
+router.get('/', packageCtrl.index)
+
+//get specific package
+router.get('/:id', packageCtrl.show)
+
+//create a package
+router.post('/', packageCtrl.create)
+
+//delete a package
+router.delete('/:id', packageCtrl.delete)
+
+//update a package
+router.put('/:id', packageCtrl.update)
+
+module.exports = router
