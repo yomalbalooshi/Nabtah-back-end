77622d8a528d9ba23915b4fdab9f587069cd2674
AliElamir <ali.elamiir.a@gmail.com>
2024-03-20 19:59:36 +0300

    create customer model

--- models/Package.js
+++ models/Package.js
@@ -4,14 +4,14 @@ const packageSchema = new Schema(
   {
     name: { type: String, required: true },
     price: { type: Number, required: true },
-    description: { type: String},
-    frequency: {type: String},
-    plants: [{ type: Schema.Types.ObjectId, ref: "Plant" }],
-    services: [{ type: Schema.Types.ObjectId, ref: "Service" }],
-    produce: [{ type: Schema.Types.ObjectId, ref: "Produce" }],
-    tools: [{ type: Schema.Types.ObjectId, ref: "Tool" }],
+    description: { type: String },
+    frequency: { type: String },
+    plants: [{ type: Schema.Types.ObjectId, ref: 'Plant' }],
+    services: [{ type: Schema.Types.ObjectId, ref: 'Service' }],
+    produce: [{ type: Schema.Types.ObjectId, ref: 'Produce' }],
+    tools: [{ type: Schema.Types.ObjectId, ref: 'Tool' }],
     availablity: { type: Boolean, required: true, default: true },
-    vendorId: [{ type: Schema.Types.ObjectId, ref: "Vendor" }]
+    vendorId: [{ type: Schema.Types.ObjectId, ref: 'Vendor' }]
   },
   { timestamps: true }
 )
