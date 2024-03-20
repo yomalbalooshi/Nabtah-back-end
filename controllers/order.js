a45f312a6d81e0edad1a4be097fbdb70f24990b8
AliElamir <ali.elamiir.a@gmail.com>
2024-03-20 22:31:35 +0300

    create order controller

---
+++ controllers/order.js
@@ -0,0 +1,19 @@
+const Order = require('../models/Order')
+
+const create = async (req, res) => {
+  // Remove empty properties so that defaults will be applied
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key]
+  }
+  try {
+    const newOrder = await Order.create(req.body)
+    res.send(newOrder)
+  } catch (err) {
+    res.send(`error in creating tool: ${err}`)
+  }
+}
+
+
+module.exports = {
+  create
+}
