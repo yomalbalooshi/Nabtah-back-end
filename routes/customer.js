30ca0ae8a35cc08603e6656fc00fe11c05939e57
AliElamir <ali.elamiir.a@gmail.com>
2024-03-20 20:28:50 +0300

    create plant controller

---
+++ routes/customer.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+const customerCtrl = require('../controllers/customer')
+
+//get all customers
+
+module.exports = router
+
+// //get all plants
+// router.get('/', plantCtrl.index)
+
+// //get specific plant
+// router.get('/:id', plantCtrl.show)
+
+// //create a plant
+// router.post('/', plantCtrl.create)
+
+// //delete a plant
+// router.delete('/:id', plantCtrl.deletePlant)
+
+// //update a plant
+// router.put('/:id', plantCtrl.update)
+
+// module.exports = router
