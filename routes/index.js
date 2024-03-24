85fd0b389d3518a68d1a3a2db92a2fcb7e586c99
AliElamir <ali.elamiir.a@gmail.com>
2024-03-24 15:37:09 +0300

    stripe payment

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+router.post('/create-checkout-session', async (req, res) => {
+  const { products } = req.body
+  //based on dummy data, need to make our cart in a similar format
+  const lineItems = products.map((product) => ({
+    price_data: {
+      currency: 'usd',
+      product_data: {
+        name: product.item
+      },
+      unit_amount: Math.round(product.price * 100)
+    },
+    quantity: 1
+    // need to get quantity from the cart
+  }))
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: lineItems,
+    mode: 'payment',
+    success_url: 'http://localhost:5173/paymentsuccess',
+    cancel_url: 'http://localhost:5173/paymentfailed'
+  })
+
+  res.json({ id: session.id })
+})
+module.exports = router
